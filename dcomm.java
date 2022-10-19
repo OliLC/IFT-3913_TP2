fefea6b0c4019c6bc67cc44c383c3855e3c1bbf3
derrick-chung <derrickch007@gmail.com>
2022-10-19 13:02:37 -0400

    Create dcomm.java
    créé la fonction de densité de commentaire pour un projet ou un fichier java

---
+++ dcomm.java
@@ -0,0 +1,90 @@
+import java.io.*;
+
+public class dcomm {
+    public static void main(String[] args) {
+        commentsDensity(args[0]);
+    }
+
+    public static float commentsDensity(String path) {
+        int[] directoryValues = directoryParser(path);
+        float density = 0;
+        if (directoryValues[1] != 0)
+            density = directoryValues[0] / directoryValues[1];
+
+        System.out.println("Comments density for "+path+": "+ density);
+        return density;
+    }
+
+    public static int[] directoryParser(String path) {
+        float density = 0;
+        int[] linesAndComments = new int[2];
+        File parent = new File(path);
+
+        if (!parent.isDirectory()) {
+            linesAndComments = fileParser(path);
+            return linesAndComments;
+        }
+        File[] children = parent.listFiles();
+        for (File i : children) {
+            if (i.isDirectory()) {
+                int[] subdirectoryValues = directoryParser(path + "/" + i.getName());
+                linesAndComments[0] += subdirectoryValues[0];
+                linesAndComments[1] += subdirectoryValues[1];
+            } else {
+                int[] fileValues = fileParser(path);
+                linesAndComments[0] += fileValues[0];
+                linesAndComments[1] += fileValues[1];
+            }
+        }
+        return linesAndComments;
+    }
+
+    public static int[] fileParser(String file) {
+        if (!file.endsWith(".java")) {
+            System.out.println("Not a java file.");
+            return new int[]{0, 0};
+        }
+        int nloc = 0;
+        int cloc = 0;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            boolean multilineComment = false;
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!"".equals(line.trim())) {
+                    nloc++;
+                    if (line.trim().endsWith("*/")) {
+                        if (multilineComment) {
+                            cloc++;
+                            multilineComment = false;
+                            continue;
+                        }
+                    }
+                    if (multilineComment) {
+                        cloc++;
+                        continue;
+                    }
+                    if (line.trim().startsWith("//")) {
+                        cloc++;
+                    }
+                    if (line.trim().startsWith("/*")) {
+                        cloc++;
+                        multilineComment = true;
+                        if (line.trim().endsWith("*/")) {
+                            multilineComment = false;
+                        }
+                    }
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("File not found");
+            return new int[]{0, 0};
+        } catch (IOException e) {
+            System.out.println("Empty line or file");
+            return new int[]{0, 0};
+        }
+        return new int[]{cloc, nloc};
+    }
+}
